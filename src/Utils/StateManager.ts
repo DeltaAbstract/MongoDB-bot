084cb269406a68dce43c24bdc1ba8f123e39d87a
ProcessVersion <51869374+ProcessVersion@users.noreply.github.com>
2021-05-09 17:09:43 -0400

    Update StateManager.ts

--- src/Utils/StateManager.ts
+++ src/Utils/StateManager.ts
@@ -2,15 +2,19 @@ import { EventEmitter } from 'events';
 import connection from '../../Database/DB';
 import { Db } from 'mongodb';
 
+function getConnection() {
+	let client: Db;
+	connection().then((con) => {
+		client = con.db();
+	});
+	return client;
+}
+
 class StateManager extends EventEmitter {
 	db: Db;
 	constructor(opts?: object) {
 		super(opts);
-	}
-	async getConnection() {
-		const client = await connection();
-		this.db = client.db();
-		return this;
+		this.db = getConnection();
 	}
 }
 
