9f2a046bcec1e47147f8817d613299a533f2a620
ProcessVersion <51869374+ProcessVersion@users.noreply.github.com>
2021-05-09 11:36:08 -0400

    Update Prefix.ts

--- src/Commands/Config/Prefix.ts
+++ src/Commands/Config/Prefix.ts
@@ -1,14 +1,31 @@
 import BaseCommand from '../../Utils/Structures/BaseCommand';
 import DiscordClient from '../../Client/Client';
 import { Message } from 'discord.js';
+import GuildSchema from '../../Utils/Structures/Schemas/GuildSchema';
 
 export default class PreifixCommand extends BaseCommand {
 	constructor() {
 		super('preifx', 'config', []);
 	}
 	async run(client: DiscordClient, message: Message, args: string[]) {
-		if (!args[0]) return message.channel.send('Missing a required argument!');
-
 		const newPrefix = args[0];
+
+		if (message.author.id != message.guild.ownerID)
+			return message.channel.send('You are not the owner!');
+
+		if (!newPrefix)
+			return message.channel.send('You must mention a new prefix');
+
+		try {
+			await GuildSchema.findOneAndUpdate(
+				{ guildId: message.guild.id },
+				{ guildId: message.guild.id, prefix: newPrefix },
+				{ upsert: true }
+			);
+			await message.channel.send(`Prefix has been set to ${newPrefix}`);
+			return;
+		} catch (error) {
+			console.log(error);
+		}
 	}
 }
