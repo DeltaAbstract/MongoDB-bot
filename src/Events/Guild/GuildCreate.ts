6f802cdd86e8ff29541e1146185c0ea5316e0dbe
ProcessVersion <51869374+ProcessVersion@users.noreply.github.com>
2021-05-09 11:16:56 -0400

    Update GuildCreate.ts

--- src/Events/Guild/GuildCreate.ts
+++ src/Events/Guild/GuildCreate.ts
@@ -1,2 +1,10 @@
 import BaseEvent from '../../Utils/Structures/BaseEvent';
 import { Guild } from 'discord.js';
+import DiscordClient from '../../Client/Client';
+
+export default class GuildCreateEvent extends BaseEvent {
+	constructor() {
+		super('guildCreate');
+	}
+	async run(client: DiscordClient, guild: Guild) {}
+}
