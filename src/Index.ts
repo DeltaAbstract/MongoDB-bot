fc80c51715a09ef95b3ee7da7de2bc0bdcaa07e5
ProcessVersion <51869374+ProcessVersion@users.noreply.github.com>
2021-05-08 22:48:19 -0400

    Learning how to save things into the DB

--- src/Index.ts
+++ src/Index.ts
@@ -1,12 +1,19 @@
 import DiscordClient from './Client/Client';
 import * as dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import User from './Utils/Schemas/UserSchema';
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then((m) => {
-	console.log("✅ | Connected to the database")
-}).catch((error) => console.log(error))
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then((m) => {
+		console.log('✅ | Connected to the database');
+	})
+	.catch((error) => console.log(error));
 
 const client = new DiscordClient({});
 
@@ -16,4 +23,13 @@ client.login(process.env.TOKEN);
 
 client.on('ready', () => {
 	console.log(`✅ | ${client.user.tag} has logged in!`);
+});
+
+client.on('message', async (message) => {
+	const newUser = await User.create({
+		username: message.author.username,
+		discordId: message.author.id,
+	});
+
+	//const savedUser = await newUser.save();
 });
--- src/Utils/Schemas/UserSchema.ts
+++ src/Utils/Schemas/UserSchema.ts
@@ -1,1 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
+
+const UserSchema = new Schema({
+	username: mongoose.SchemaTypes.String,
+	discordId: {
+		type: mongoose.SchemaTypes.String,
+		required: true,
+	},
+});
+
+export = mongoose.model('User', UserSchema);
